836693f78aebc3e01bc6e469aa046023754dc32e
Rishabh <56164824+rishabhsdev@users.noreply.github.com>
2023-11-14 10:38:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+import { typeDefs } from "./schema.js";
+import { resolvers } from "./resolvers.js";
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 3000 },
+});
+
+console.log(`Server ready at: ${url}`);
